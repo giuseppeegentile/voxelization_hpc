e7f17fa2cfb868c9a2671b13b5787f4265170716
lucamuscarnera <48683941+lucamuscarnera@users.noreply.github.com>
2023-03-10 19:10:33 +0100

    Aggiunta classe rappresentazione intermedia

---
+++ src/Main/Parser/IR.h
@@ -0,0 +1,14 @@
+/* IR.h */
+//		Fornisce un container per la rappresentazione intermedia
+// 		Accessibile poi per costruire la rappresentazione nella struttura
+//		Ottimizzata
+
+#ifndef IR_H
+#define IR_H
+
+class IR {
+	public:
+		IR() {};
+};
+
+#endif
--- src/Main/Parser/Parser.h
+++ src/Main/Parser/Parser.h
@@ -1,1 +1,23 @@
 /* Parser.h */
+#ifndef PARSER_H
+#define PARSER_H
+#include <fstream>
+#include "IR.h"
+
+class Parser
+{
+	public:
+		Parser( const char * filename )  : file( filename , std::ifstream::in) {
+			
+			// legge il file riga per riga riempiendo la struttura intermedia
+			
+			// fine
+			
+		}; 
+		
+	private:
+		std::ifstream file;
+		IR intermediateRepresentation;
+};
+
+#endif
