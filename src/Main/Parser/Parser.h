00be21005512280b7864b08094e173fde05a19f0
gen7 <giuseppe2.gentile@mail.polimi.it>
2023-03-11 12:04:06 +0100

    Piccola ottimizzazione parser + file strutture proteine

--- src/Main/Parser/Parser.h
+++ src/Main/Parser/Parser.h
@@ -18,32 +18,27 @@ class Parser
 		Parser( const char * filename ) {
 			// IN:		nome del file da leggere
 			// OUT:		costruisce la rappresentazione intermedia
+			// apre il file
+			std::ifstream file( filename , std::ifstream::in);
+			// inizializza una stringa che andrá a contenere la riga
+			std::string line;
+			// Vai fino a dove non trovo ATOM, ovvero primo carattere A
+			while (std::getline(file, line) && line[0] != 'A');
 			
-			
-				// apre il file
-				std::ifstream file( filename , std::ifstream::in);
-				// inizializza una stringa che andrá a contenere la riga
-				std::string line;
-				// fino a quando ci sono righe da leggere...
-				while (std::getline(file, line))
-				{
-					if( line.substr(0,5) == "ATOM " )
-					{
-						// SOLUZIONE BRUTTA: leggo dopo il carattere 28 che sono sicuro ci stanno i double
-						std::string data = line.substr(28, line.length());
-						std::stringstream streamer(data);
-						float x,y,z;
-						// SALVO I VALORI LOCALMENTE
-						streamer >> x;
-						streamer >> y;
-						streamer >> z;
-						// AGGIORNO LA STRUTTURA DATI
-						intermediateRepresentation.push(x,y,z);
-					}
-				}
-			// fine
-			
-			
+			// Arrivato agli ATOM, leggi le coordinate 
+			while(std::getline(file, line) && line[0] == 'A') {
+				// SOLUZIONE BRUTTA: leggo dopo il carattere 28 che sono sicuro ci stanno i double
+				std::string data = line.substr(28, line.length());
+				std::stringstream streamer(data);
+				float x,y,z;
+				// SALVO I VALORI LOCALMENTE
+				streamer >> x;
+				streamer >> y;
+				streamer >> z;
+				// AGGIORNO LA STRUTTURA DATI
+				intermediateRepresentation.push(x,y,z);
+			}
+			// fine	
 		}; 
 	
 	IR & getIR();
